a9daf1c793f637e71e55c0e8d04a1d012b5f13c0
Nikos Mpakaris <57401398+mpakaris@users.noreply.github.com>
2024-10-08 19:43:13 +0200

    Selected Marker on Map changes Color

--- src/app/components/Maps/MapTiler.jsx
+++ src/app/components/Maps/MapTiler.jsx
@@ -1,42 +1,66 @@
 import * as maptilersdk from '@maptiler/sdk';
 import "@maptiler/sdk/dist/maptiler-sdk.css";
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import '../../styles/map.css';
 
 const Map = ({ onMarkerClick, locations }) => {
   const mapContainer = useRef(null);
   const map = useRef(null);
+  const [markers, setMarkers] = useState([]);
+  const [selectedMarkerUuid, setSelectedMarkerUuid] = useState(null);
   const location = { lat: 47.49919, lng: 19.0527 };
   const zoom = 11;
   maptilersdk.config.apiKey = 'bKNG0Ir1ORmL8bGkuCNM';
 
-  useEffect(() => {
-    if (map.current) return;
+  const returnCorrectColor = (uuid) => {
+    return selectedMarkerUuid === uuid ? '#DF245B' : '#00695C';
+  };
 
-    map.current = new maptilersdk.Map({
-      container: mapContainer.current,
-      // Use the Streets Dark style
-      // style: "https://api.maptiler.com/maps/streets-dark/style.json?key=bKNG0Ir1ORmL8bGkuCNM",
-      center: [location.lng, location.lat],
-      zoom: zoom
-    });
+  // Function to clear markers from the map
+  const clearMarkers = () => {
+    markers.forEach((marker) => marker.remove());
+    setMarkers([]);
+  };
 
-    locations.forEach((loc) => {
+  // Function to add markers to the map
+  const addMarkers = () => {
+    const newMarkers = locations.map((loc) => {
       const marker = new maptilersdk.Marker({
-        // color: "#FFFFFF"
+        color: returnCorrectColor(loc.uuid),
       })
         .setLngLat([loc.lng, loc.lat])
         .addTo(map.current);
 
-      marker.getElement().addEventListener("click", () => onMarkerClick(loc.uuid));
+      // Add click event to marker
+      marker.getElement().addEventListener("click", () => {
+        setSelectedMarkerUuid(loc.uuid);
+        onMarkerClick(loc.uuid);
+      });
+
+      return marker;
     });
-  }, [location.lng, location.lat, zoom, locations, onMarkerClick]);
+    setMarkers(newMarkers);
+  };
+
+  useEffect(() => {
+    if (!map.current) {
+      map.current = new maptilersdk.Map({
+        container: mapContainer.current,
+        center: [location.lng, location.lat],
+        zoom: zoom,
+      });
+    }
+
+    // Clear existing markers and add new ones whenever selectedMarkerUuid changes
+    clearMarkers();
+    addMarkers();
+  }, [selectedMarkerUuid, locations]);
 
   return (
     <div className="map-wrap">
       <div ref={mapContainer} className="map" />
     </div>
   );
-}
+};
 
 export default Map;
